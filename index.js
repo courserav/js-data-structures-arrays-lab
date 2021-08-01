1a160d1234033befb0ef314e6e5cb17022ec23c1
courserav <lvsmlit@gmail.com>
2021-08-01 13:31:25 -0400

    commit tree a075d7cebac153a503a40a783424b2e42c138b9a

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+let drivers = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendDriver(name){
+
+    drivers.push(name)
+}
+
+function destructivelyPrependDriver(name){
+    drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(){
+    drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(){
+    drivers.shift()
+}
+
+function appendDriver(name){
+    let apDrivers = drivers
+    apDrivers = [...apDrivers, name]
+    return apDrivers
+}
+
+function prependDriver(name){
+    let prepDrivers = drivers.slice()
+    prepDrivers.unshift(name)
+    return prepDrivers
+}
+
+function removeLastDriver(){
+    return drivers.slice(0, -1)
+}
+
+function removeFirstDriver(){
+    return drivers.slice(1)
+}
